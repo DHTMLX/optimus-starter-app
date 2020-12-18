5210af8d5337f212e174ba5e423fb01b623f050b
Sorokin-Oleg <sorokin.world@gmail.com>
2020-12-18 11:09:04 +0300

    [dev] some fixed

--- src/views/EmptyView.js
+++ src/views/EmptyView.js
@@ -2,6 +2,6 @@ import { View } from "dhx-optimus";
 
 export class EmptyView extends View {
 	init() {
-		return `<div class="empty__container">This is ${this.params.content || "empty"} view</div>`;
+		return `<div class="empty__container">This is the ${this.params.content || "empty"} view</div>`;
 	}
 }
